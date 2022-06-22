174025965298915622c4cfaae0d7e5c1b9075286
Antoine Piron <antoine.pironsio@gmail.com>
2022-06-22 16:30:17 +0200

    end loader

--- R&D/app.js
+++ R&D/app.js
@@ -1,24 +1,37 @@
 let test = 
-`<header>
-    <h1 class="formateur papermaker starcraft">Alain</h1>
-</header>
-<section>
-    <div class="exploratice statisfactory jeanine ">Pauline</div>
-    <div class="EldenLord VTTiste ">Julien</div>
-    <div>Bernard</div>
-    <div>Tiana</div>
-    <div class="nature travail">Chris</div>
-    <div>Jorge</div>
-    <div>Hamza</div>
-    <div>Karolos</div>
-    <div>Vincent</div>
-    <div class="theRat">Natan</div>
-    <div class ="satisfactory organise automatise">Léo</div>
-    <div class="civilisation satisfactory weeb">Antoine</div>
-</section>
-<footer>
-    <span id="en flamme">nos ordinateurs</span>
-</footer>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PopCode</title>
+</head>
+<body>
+    <header>
+        <h1 class="formateur papermaker starcraft">Alain</h1>
+    </header>
+    <section>
+        <div class="exploratice satisfactory jeanine ">Pauline</div>
+        <div class="EldenLord VTTiste ">Julien</div>
+        <div>Bernard</div>
+        <div>Tiana</div>
+        <div class="nature travail">Chris</div>
+        <div>Jorge</div>
+        <div>Hamza</div>
+        <div>Karolos</div>
+        <div>Vincent</div>
+        <div class="theRat">Natan</div>
+        <div class ="satisfactory organise automatise">Léo</div>
+        <div class="civilisation satisfactory weeb">Antoine</div>
+    </section>
+    <footer>
+        <span id="en flamme">nos ordinateurs</span>
+    </footer>
+    <script src="app.js"></script>
+</body>
+</html> 
 `
 //function typing text
 function typingText(text, speed) {
