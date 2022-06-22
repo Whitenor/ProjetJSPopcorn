a7517e60653c19cdeeb2244bc0c8e7d6c03ebd92
Antoine Piron <antoine.pironsio@gmail.com>
2022-06-22 15:29:06 +0200

    loader mostly finishi , time to add more content for it

--- R&D/app.js
+++ R&D/app.js
@@ -1,10 +1,10 @@
 let test = 
 `<header>
-    <h1 class="formateur paper maker starcraft">Alain</h1>
+    <h1 class="formateur papermaker starcraft">Alain</h1>
 </header>
 <section>
     <div class="exploratice statisfactory jeanine ">Pauline</div>
-    <div class="Elden Lord VTTiste ">Julien</div>
+    <div class="EldenLord VTTiste ">Julien</div>
     <div>Bernard</div>
     <div>Tiana</div>
     <div class="nature travail">Chris</div>
@@ -12,12 +12,12 @@ let test =
     <div>Hamza</div>
     <div>Karolos</div>
     <div>Vincent</div>
-    <div>Natan</div>
+    <div class="theRat">Natan</div>
     <div class ="satisfactory organise automatise">Léo</div>
     <div class="civilisation satisfactory weeb">Antoine</div>
 </section>
 <footer>
-    <span>nos ordinateurs</span>
+    <span id="en flamme">nos ordinateurs</span>
 </footer>
 `
 //function typing text
