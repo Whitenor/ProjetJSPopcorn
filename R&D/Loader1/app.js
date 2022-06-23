d1fd2150316693a79a5ae797aaf314028896d6f8
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-06-23 14:14:18 +0200

    update loader, need some fix

--- R&D/Loader1/app.js
+++ R&D/Loader1/app.js
@@ -1,10 +1,10 @@
 let test = 
 `<!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="C'est l'histoire d'un développeur web et de 12 futur développeur web qui
+     se sont mis en tête de faire rayonner l'ACS de Lons le Saunier">
     <link rel="stylesheet" href="style.css">
     <title>PopCode</title>
 </head>
@@ -45,4 +45,4 @@ function typingText(text, speed) {
         }
     }, speed);
 }
-typingText(test, 50);
+typingText(test, 15);
