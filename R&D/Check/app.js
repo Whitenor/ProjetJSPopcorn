57ea86dcb1c357f40a8067eb2979ed08d293d144
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-06-27 14:56:34 +0200

    update check , add some secondary system

--- R&D/Check/app.js
+++ R&D/Check/app.js
@@ -3,38 +3,80 @@ var testInput = document.querySelector('#testInput');
 var retour = document.querySelector('#return');
 var playground = document.querySelector('.playGround');
 var main = document.querySelector('.main');
+var modal = document.querySelector('.modalTest');
+var minInput = testInput.value.toLowerCase();
 var score = 0;
 var erreur = 0;
 const guessing = ['html', 'css', 'js'];
+var found = [];
+
+
+function checkScore() {
+    if (score === 2) {
+        retour.innerHTML += '<p>'+testInput.value+'</p>';
+        retour.innerHTML += "<strong>Vous avez gagné !</strong>";
+    }
+    else{
+        retour.innerHTML += '<p>'+testInput.value+'</p>';
+        testInput.value = '';
+        score++;
+    }
+}
+function hide(){
+    main.classList.remove('hide');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 testSubmit.addEventListener('click', function(e) {
-    var minInput = testInput.value.toLowerCase();
+    minInput = testInput.value.toLowerCase();
     for (let i = 0; i < guessing.length; i++) {
         if (minInput === guessing[i]) {
-            if (score === 2) {
-                retour.innerHTML += '<p>'+guessing[i]+'</p>';
-                retour.innerHTML += "<strong>Vous avez gagné !</strong>";
-            }
-            else{
-                retour.innerHTML += '<p>'+guessing[i]+'</p>';
-                testInput.value = '';
-            }
-            score++;
+            checkScore();
             main.classList.add('hide');
             return;
         } else if (i === 2) {
             if (erreur === 2) {
-                document.querySelector('body').innerHTML = "Vous avez perdu !";
+                retour.innerHTML = "<p>Vous avez perdu !</p>";
+                playground.removeEventListener('click', hide);
+                console.log("test")
             }
             else{
-                retour.innerHTML = 'Dommage !';
                 testInput.value = '';
+                erreur++;
             }
-            erreur++;
+            main.classList.add('hide');
         }
+        console.log(erreur)
     }
 })
-playground.addEventListener('click', function(e) {
-    main.classList.remove('hide');
+// modal.addEventListener('click', function(e) {
+//         main.classList.add('hide');
+// })
+playground.addEventListener('click', hide);
+window.addEventListener('keydown', function(e) {
+    console.log(e.key);
+    if (e.code === 'KeyR') {
+        testInput.value+=e.key;
+    }
 })
 
