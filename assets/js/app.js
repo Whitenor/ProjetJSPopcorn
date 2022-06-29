9f7829d8dcd5dd284870a0081ba1182af7c1347d
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-06-29 13:06:00 +0200

    start integrate

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+let test = 
+`<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="C'est l'histoire d'un développeur web et de 12 futur développeur web qui
+     se sont mis en tête de faire rayonner l'ACS de Lons le Saunier">
+    <link rel="stylesheet" href="style.css">
+    <title>popCode</title>
+</head>
+<body>
+    <header>
+        <h1 class="formateur papermaker starcraft">Alain</h1>
+    </header>
+    <section>
+        <div class="exploratrice satisfactory jeanine ">Pauline</div>
+        <div class="EldenLord VTTiste ">Julien</div>
+        <div>Bernard</div>
+        <div>Tiana</div>
+        <div class="nature travail">Chris</div>
+        <div>Jorge</div>
+        <div>Hamza</div>
+        <div>Karolos</div>
+        <div>Vincent</div>
+        <div class="theRat">Natan</div>
+        <div class ="satisfactory organise automatise">Léo</div>
+        <div class="civilisation satisfactory weeb">Antoine</div>
+    </section>
+    <footer>
+        <span id="en flamme">nos ordinateurs</span>
+    </footer>
+    <script src="app.js"></script>
+</body>
+</html> 
+`
+//function typing text
+function typingText(text, speed) {
+    var i = 0;
+    var timer = setInterval(function () {
+        if (i < text.length) {
+            document.getElementById("typed").innerHTML += text.charAt(i);
+            i++;
+        } else {
+            clearInterval(timer);
+            setTimeout(() => {
+                document.querySelector('#typed').remove();
+            }, 2000);
+        }
+    }, speed);
+}
+typingText(test, 8);
