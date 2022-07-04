da403f296e6c57eddf5959f2194ed399783d7ff2
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-07-04 16:15:46 +0200

    update on maingame , need to fix repetition of an answer counting in score. Need modal for descript language

--- R&D/Check/app.js
+++ R&D/Check/app.js
@@ -1,8 +1,8 @@
-var testInput = document.querySelector('#testInput');
+var Input = document.querySelector('#Input');
 var retour = document.querySelector('#return');
 var overlay = document.querySelector('#overlay');
-var modal = document.querySelector('.modalTest');
-var minInput = testInput.innerHTML.toLowerCase();
+var overlay = document.querySelector('.overlay');
+var minInput = Input.innerHTML.toLowerCase();
 var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Semicolon', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var score = 0;
 var erreur = 0;
@@ -11,29 +11,27 @@ const guessing = ['HTML', 'CSS', 'JavaScript', 'SQL', 'Python', 'Java', 'Bash', 
 const guessingMin = []
 function checkScore(checkNow) {
     if (score === 2) {
-        retour.innerHTML += '<p>'+checkNow+'</p>';
-        retour.innerHTML += "<strong>Vous avez gagné !</strong>";
+        return;
     }
     else{
-        retour.innerHTML += '<p>'+checkNow+'</p>';
-        testInput.innerHTML = '';
+        Input.innerHTML = '';
         found[score] = minInput;
         score++;
     }
 }
-function removeHide(){
-    overlay.classList.remove('hide');
+function removenone(){
+    overlay.classList.remove('none');
 }
-function hide(){
-    overlay.classList.add('hide');
+function none(){
+    overlay.classList.add('none');
 }
 for (let a = 0; a < guessing.length; a++) {
     guessingMin[a] = guessing[a].toLowerCase(); 
 }
 
 overlay.addEventListener('click', function(){
-    if (testInput.innerHTML ==='') {
-        hide();
+    if (Input.innerHTML ==='') {
+        none();
     }
 });
 
@@ -42,23 +40,23 @@ window.addEventListener('keydown', function checking(e) {
     if (erreur ===3){
         return;
     }
-    if (testInput.innerHTML !== '') {
+    if (Input.innerHTML !== '') {
         if (e.code === "Enter"){
-            minInput = testInput.innerHTML.toLowerCase();
+            minInput = Input.innerHTML.toLowerCase();
             if (found.length !== 0){
-                for (var i = 0; i < found.length; i++) {
-                    if (minInput === found[i]) {
-                        retour.innerHTML += '<p> vous avez déjà trouvé '+found[i]+'</p>';
-                        overlay.classList.add('hide');
-                        testInput.innerHTML = '';
-                        return;
-                    }
-                }
+                // for (var i = 0; i < found.length; i++) {
+                //     if (minInput === found[i]) {
+                //         retour.innerHTML += '<p> vous avez déjà trouvé '+found[i]+'</p>';
+                //         overlay.classList.add('none');
+                //         Input.innerHTML = '';
+                //         return;
+                //     }
+                // }
             }
             for (let i = 0; i < guessing.length; i++) {
                 if (minInput === guessingMin[i]) {
                     checkScore(guessing[i]);
-                    overlay.classList.add('hide');
+                    overlay.classList.add('none');
                     return;
                 } else if (i === 2) {
                     if (erreur === 2) {
@@ -66,23 +64,23 @@ window.addEventListener('keydown', function checking(e) {
                         erreur++;
                     }
                     else{
-                        testInput.innerHTML = '';
+                        Input.innerHTML = '';
                         erreur++;
                     }
-                    overlay.classList.add('hide');
+                    overlay.classList.add('none');
                 }
             }
         }
         if (e.code ==='Backspace'){
-            testInput.innerHTML = testInput.innerHTML.slice(0, -1);
+            Input.innerHTML = Input.innerHTML.slice(0, -1);
         }    
     }
     for (let z = 0; z < letters.length; z++) {
         if (e.code === "Key"+letters[z] || e.code === letters[z]) {
-            if(overlay.classList[0] === 'hide'){
-            removeHide();
+            if(overlay.classList[0] === 'none'){
+            removenone();
             }
-            document.querySelector('#testInput').innerHTML += e.key
+            document.querySelector('#Input').innerHTML += e.key
         }
     }
 })
--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,19 @@ var typed =document.getElementById("typed");
 var header = document.querySelector('header');
 var launchGame = document.querySelector('#launchGame');
 var mainGame = document.querySelector('.mainGame');
-var testPlus = document.querySelector('.plus');
-var testMin = document.querySelector('.minx');
+var Plus = document.querySelector('.plus');
+var Min = document.querySelector('.minx');
+var Input = document.querySelector('#Input');
+var retour = document.querySelector('#return');
+var overlay = document.querySelector('#overlay');
+var countFound = document.querySelector('.countFound');
+var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Semicolon', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var checkCountRotate = 0;
+var score = 0;
+var erreur = 0;
+var found = [];
+const guessing = ['HTML', 'CSS', 'JavaScript', 'SQL', 'Python', 'Java', 'Bash', 'Shell', 'PowerShell','C#','PHP', 'C++', 'Typescript', 'C', 'Ruby','Go','Assembly','Swift','Kotlin','R','VBA','Objective-C','Scala','Rust','Dart','Elixir','Clojure','WebAssembly'];
+const guessingMin = []
 var testWidth = 50;
 var errorCountTest = 1;
 let loader = 
@@ -40,10 +51,11 @@ let loader =
     <script src="app.js"></script>
 </body>
 </html> 
-`
+`;
+var minInput = Input.innerHTML.toLowerCase();
 //function typing text
 function typingText(text, speed) {
-    var i = 0;
+    let i = 0;
     var timer = setInterval(function () {
         if (i < text.length) {
             typed.innerHTML += text.charAt(i);
@@ -57,17 +69,98 @@ function typingText(text, speed) {
         }
     }, speed);
 }
+function checkScore() {
+    if (score === guessing.length - 1) {
+        return;
+    }
+    else{
+        Input.innerHTML = '';
+        found[score] = minInput;
+        score++;
+        countFound.innerHTML = score;
+    }
+}
+function removenone(){
+    overlay.classList.remove('none');
+}
+function none(){
+    overlay.classList.add('none');
+}
+for (let a = 0; a < guessing.length; a++) {
+    guessingMin[a] = guessing[a].toLowerCase(); 
+}
 typingText(loader, 1);
 launchGame.addEventListener('click', function () {
     header.remove();
     mainGame.classList.remove('none');
+})
+overlay.addEventListener('click', function(){
+    if (Input.innerHTML ==='') {
+        none();
+    }
+});
+
+window.addEventListener('keydown', function checking(e) {
+    console.log(e.code);
+    if (erreur ===3){
+        return;
+    }
+    if (Input.innerHTML !== '') {
+        if (e.code === "Enter"){
+            minInput = Input.innerHTML.toLowerCase();
+            checkCountRotate = 0;
+            for (let i = 0; i < guessing.length; i++) {
+                if (minInput === guessingMin[i]) {
+                    checkScore();
+                    overlay.classList.add('none');
+                    return;
+                }
+                checkCountRotate++;
+            }
+            console.log(checkCountRotate);
+            if (checkCountRotate === guessing.length) {
+                {
+                    switch(erreur){
+                        case 0:
+                            Input.innerHTML = '';
+                            erreur++;
+                            document.querySelector('.error1').classList.add('lightBlue');
+                            break;
+                        case 1:
+                            Input.innerHTML = '';
+                            erreur++;
+                            document.querySelector('.error2').classList.add('lightBlue');
+                            break;
+                        case 2:
+                            Input.innerHTML = '';
+                            erreur++;
+                            document.querySelector('.error3').classList.add('lightBlue');
+                            break;
+                    }
+                    overlay.classList.add('none');
+                }
+            }
+        }
+        if (e.code ==='Backspace'){
+            Input.innerHTML = Input.innerHTML.slice(0, -1);
+        }    
+    }
+    for (let z = 0; z < letters.length; z++) {
+        if (e.code === "Key"+letters[z] || e.code === letters[z] || e.key === '-') {
+            if(overlay.classList[0] === 'none'){
+            removenone();
+            }
+            document.querySelector('#Input').innerHTML += e.key
+            return;
+        }
+    }
 })
 
 
 // Legacy Code
 
 
-// testPlus.addEventListener('click', function(){
+// Plus.addEventListener('click', function(){
 //     testWidth = testWidth + 50;
 //     document.querySelector('.fill').style.width = testWidth + 'px';
 // })
